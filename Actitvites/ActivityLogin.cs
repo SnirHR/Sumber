f03a6af33ba48687877ad0c38d6a7dbb1a11603a
S-R <SnirHR.dev@gmail.com>
2023-01-17 15:14:19 +0200

    Add project files.

---
+++ Actitvites/ActivityLogin.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bagrot.Actitvites
+{
+    [Activity(Label = "ActivityLogin")]
+    public class ActivityLogin : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.layoutLogin);
+            // Create your application here
+        }
+    }
+}
